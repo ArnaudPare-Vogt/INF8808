480316ad85b4e78e38218fb819e68037e6457822
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-03-26 13:08:30 -0400

    Add data preprocessing

--- Project/src/main.js
+++ Project/src/main.js
@@ -6,5 +6,5 @@ let example_flight_file = new ULogFile("example_flight");
 example_flight_file.retreive_message("vehicle_global_position_0")
   .then(console.log);
 
-example_flight_file.retreive_message("sensor_accel_0")
+example_flight_file.retreive_message("actuator_outputs_0")
   .then(console.log);
--- Project/src/preprocessing.js
+++ Project/src/preprocessing.js
@@ -15,6 +15,49 @@ const message_preprocessors = {
     row.terrain_alt_valid = parseInt(row.terrain_alt_valid) !== 0;
     row.dead_reckoning = parseInt(row.dead_reckoning) !== 0;
   },
+  "vehicle_gps_position_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/vehicle_gps_position.msg
+    row.lat = parseInt(row.lat) * 1e-7;
+    row.lon = parseInt(row.lon) * 1e-7;
+
+    row.alt = parseInt(row.alt) * 1e-3;
+    row.alt_ellipsoid = parseFloat(row.alt_ellipsoid) * 1e-3;
+
+    row.s_variance_m_s = parseFloat(row.s_variance_m_s);
+    row.c_variance_rad = parseFloat(row.c_variance_rad);
+
+    row.fix_type = parseInt(row.fix_type);
+    
+    row.eph = parseFloat(row.eph);
+    row.epv = parseFloat(row.epv);
+    
+    row.hdop = parseFloat(row.hdop);
+    row.vdop = parseFloat(row.vdop);
+
+    row.noise_per_ms = parseInt(row.noise_per_ms);
+    row.jamming_indicator = parseInt(row.jamming_indicator);
+
+    row.vel_m_s = parseFloat(row.vel_m_s);
+    row.vel_n_m_s = parseFloat(row.vel_n_m_s);
+    row.vel_e_m_s = parseFloat(row.vel_e_m_s);
+    row.vel_d_m_s = parseFloat(row.vel_d_m_s);
+    row.cog_rad = parseFloat(row.cog_rad);
+    row.vel_ned_valid = parseInt(row.vel_ned_valid) !== 0;
+
+    row.satellites_used = parseInt(row.satellites_used);
+
+    row.heading = parseFloat(row.heading);
+    row.heading_offset = parseFloat(row.heading_offset);
+  },
+  "vehicle_land_detected_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/vehicle_land_detected.msg
+    row.alt_max = parseFloat(row.alt_max);
+    row.freefall = parseInt(row.freefall) !== 0;
+    row.ground_contact = parseInt(row.ground_contact) !== 0;
+    row.maybe_landed = parseInt(row.maybe_landed) !== 0;
+    row.landed = parseInt(row.landed) !== 0;
+    row.in_ground_effect = parseInt(row.in_ground_effect) !== 0;
+  },
   "sensor_accel_0": (row, parse_timestamp) => {
     // See https://github.com/PX4/Firmware/blob/master/msg/sensor_accel.msg
     row.timestamp_sample = parse_timestamp(row.timestamp_sample);
@@ -24,7 +67,151 @@ const message_preprocessors = {
     row.z = parseFloat(row.z);
     row.temperature = parseFloat(row.temperature);
   },
+  "sensor_baro_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/sensor_baro.msg
+    row.device_id = parseInt(row.device_id);
+    row.error_count = parseInt(row.error_count);
+    row.pressure = parseFloat(row.pressure);
+    row.temperature = parseFloat(row.temperature);
+  },
+  "sensor_mag_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/sensor_mag.msg
+    row.device_id = parseInt(row.device_id);
+    row.error_count = parseInt(row.error_count);
+    row.x = parseFloat(row.x);
+    row.y = parseFloat(row.y);
+    row.z = parseFloat(row.z);
+    row.temperature = parseFloat(row.temperature);
+    row.scaling = parseFloat(row.scaling);
+    row.x_raw = parseInt(row.x_raw);
+    row.y_raw = parseInt(row.y_raw);
+    row.z_raw = parseInt(row.z_raw);
+    row.is_external = parseInt(row.is_external) !== 0;
+  },
+  "sensor_gyro_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/sensor_gyro.msg
+    row.timestamp_sample = parse_timestamp(row.timestamp_sample);
+    row.device_id = parseInt(row.device_id);
+    row.x = parseFloat(row.x);
+    row.y = parseFloat(row.y);
+    row.z = parseFloat(row.z);
+    row.temperature = parseFloat(row.temperature);
+  },
+  "cpuload_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/cpuload.msg
+    row.load = parseFloat(row.load);
+    row.ram_usage = parseFloat(row.ram_usage);
+  },
+  "battery_status_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/battery_status.msg
+    row.voltage_v = parseFloat(row.voltage_v);
+    row.voltage_filtered_v = parseFloat(row.voltage_filtered_v);
+    row.current_a = parseFloat(row.current_a);
+    row.current_filtered_a = parseFloat(row.current_filtered_a);
+    row.average_current_a = parseFloat(row.average_current_a);
+    row.discharged_mah = parseFloat(row.discharged_mah);
+    row.remaining = parseFloat(row.remaining);
+    row.scale = parseFloat(row.scale);
+    row.temperature = parseFloat(row.temperature);
+    row.cell_count = parseInt(row.cell_count);
+
+    // most other fields are not interresting
+  },
+  "input_rc_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/input_rc.msg
+    row.timestamp_last_signal = parse_timestamp(row.timestamp_last_signal);
+    row.channel_count = parseInt(row.channel_count);
+    row.rssi = parseInt(row.rssi);
+    
+    row.rc_failsafe = parseInt(row.rc_failsafe) !== 0;
+    row.rc_lost = parseInt(row.rc_lost) !== 0;
+    row.rc_lost_frame_count = parseInt(row.rc_lost_frame_count);
+    row.rc_total_frame_count = parseInt(row.rc_total_frame_count);
+    row.rc_ppm_frame_length = parseInt(row.rc_ppm_frame_length);
+
+    row.input_source = parseInt(row.input_source);
+    row.values = [
+      parseInt(row["values[0]"]),
+      parseInt(row["values[1]"]),
+      parseInt(row["values[2]"]),
+      parseInt(row["values[3]"]),
+      parseInt(row["values[4]"]),
+      parseInt(row["values[5]"]),
+      parseInt(row["values[6]"]),
+      parseInt(row["values[7]"]),
+      parseInt(row["values[8]"]),
+      parseInt(row["values[9]"]),
+      parseInt(row["values[10]"]),
+      parseInt(row["values[11]"]),
+      parseInt(row["values[12]"]),
+      parseInt(row["values[13]"]),
+      parseInt(row["values[14]"]),
+      parseInt(row["values[15]"]),
+      parseInt(row["values[16]"]),
+      parseInt(row["values[17]"])
+    ];
+    delete row["values[0]"];
+    delete row["values[1]"];
+    delete row["values[2]"];
+    delete row["values[3]"];
+    delete row["values[4]"];
+    delete row["values[5]"];
+    delete row["values[6]"];
+    delete row["values[7]"];
+    delete row["values[8]"];
+    delete row["values[9]"];
+    delete row["values[10]"];
+    delete row["values[11]"];
+    delete row["values[12]"];
+    delete row["values[13]"];
+    delete row["values[14]"];
+    delete row["values[15]"];
+    delete row["values[16]"];
+    delete row["values[17]"];
+  },
+  "actuator_outputs_0": (row, parse_timestamp) => {
+    // See https://github.com/PX4/Firmware/blob/master/msg/actuator_outputs.msg
+    row.noutputs = parseInt(row.noutputs);
+    row.output = [
+      parseInt(row["output[0]"]),
+      parseInt(row["output[1]"]),
+      parseInt(row["output[2]"]),
+      parseInt(row["output[3]"]),
+      parseInt(row["output[4]"]),
+      parseInt(row["output[5]"]),
+      parseInt(row["output[6]"]),
+      parseInt(row["output[7]"]),
+      parseInt(row["output[8]"]),
+      parseInt(row["output[9]"]),
+      parseInt(row["output[10]"]),
+      parseInt(row["output[11]"]),
+      parseInt(row["output[12]"]),
+      parseInt(row["output[13]"]),
+      parseInt(row["output[14]"]),
+      parseInt(row["output[15]"])
+    ];
+    delete row["output[0]"];
+    delete row["output[1]"];
+    delete row["output[2]"];
+    delete row["output[3]"];
+    delete row["output[4]"];
+    delete row["output[5]"];
+    delete row["output[6]"];
+    delete row["output[7]"];
+    delete row["output[8]"];
+    delete row["output[9]"];
+    delete row["output[10]"];
+    delete row["output[11]"];
+    delete row["output[12]"];
+    delete row["output[13]"];
+    delete row["output[14]"];
+    delete row["output[15]"];
+  },
 };
+
+
+
+
 
 class ULogFile {
   /**
