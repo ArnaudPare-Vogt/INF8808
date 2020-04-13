717dd421461a9f06be589752aa7f5a43660f4992
Arnaud Paré-Vogt <arnaud.parevogt@gmail.com>
2020-04-13 13:34:57 -0400

    Add some useful util functions

--- Project/src/utils.js
+++ Project/src/utils.js
@@ -30,3 +30,78 @@ function lat_lon_distance_to_meters(lat_1, lat_2, lon_1, lon_2) {
   let east = EARTH_RADIUS * Math.cos(lat_1 * Math.PI / 180) * (lon_2 - lon_1) * Math.PI / 180;
   return [east, north];
 }
+
+
+
+
+
+// From a later version of d3-array
+// https://github.com/d3/d3-array/blob/master/src/minIndex.js
+Array.prototype.minIndex = function(valueof) {
+  let values = this;
+  let min;
+  let minIndex = -1;
+  let index = -1;
+  if (valueof === undefined) {
+    for (const value of values) {
+      ++index;
+      if (value != null
+          && (min > value || (min === undefined && value >= value))) {
+        min = value, minIndex = index;
+      }
+    }
+  } else {
+    for (let value of values) {
+      if ((value = valueof(value, ++index, values)) != null
+          && (min > value || (min === undefined && value >= value))) {
+        min = value, minIndex = index;
+      }
+    }
+  }
+  return minIndex;
+}
+
+
+
+
+
+// From a later version of d3-array
+// https://github.com/d3/d3-array/blob/master/src/maxIndex.js
+Array.prototype.maxIndex = function(valueof) {
+  let values = this;
+  let max;
+  let maxIndex = -1;
+  let index = -1;
+  if (valueof === undefined) {
+    for (const value of values) {
+      ++index;
+      if (value != null
+          && (max < value || (max === undefined && value >= value))) {
+        max = value, maxIndex = index;
+      }
+    }
+  } else {
+    for (let value of values) {
+      if ((value = valueof(value, ++index, values)) != null
+          && (max < value || (max === undefined && value >= value))) {
+        max = value, maxIndex = index;
+      }
+    }
+  }
+  return maxIndex;
+}
+
+
+
+
+
+/**
+ * The range function from python, implemented in javascript.
+ * This allows easy creation of arrays. For example, the following
+ * creates an array of capital letters for the alphabet:
+ * ```range(26).map(i => 'A' + i)```
+ * @param {number} max the (exclusive) last index of the range
+ */
+function range(max) {
+  return [...Array(max).keys()];
+}
