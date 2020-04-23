11a1ca0fe9a5b6313847bd1cb0f0376aaef32cd5
Riad <combatarms970711@gmail.com>
2020-04-23 14:18:00 -0400

    Finally showing some graphs in the sensor tab

--- Project/src/sensor-tab.js
+++ Project/src/sensor-tab.js
@@ -1,11 +1,11 @@
 const CIRCLE_RADIUS = 45;
-let sensors = [{ "id": 0, "name": 'Accelerator' },
-{ "id": 1, "name": 'Estimated' },
-{ "id": 2, "name": 'GPS' },
-{ "id": 3, "name": 'Battery' },
-{ "id": 4, "name": 'Wind' },
-{ "id": 5, "name": 'Baro' },
-{ "id": 6, "name": 'Gyro' }]
+let sensors = [{ "id": 0, "name": 'Accelerator', "sensor_id": "sensor_accel_0", "data_getter": d => d.mag },
+{ "id": 1, "name": 'Estimated', "sensor_id": "vehicle_global_position_0", "data_getter": d => d.enu.u }, //TODO: Check
+{ "id": 2, "name": 'GPS', "sensor_id": "vehicle_gps_position_0", "data_getter": d => d.alt }, //TODO: Check getter
+{ "id": 3, "name": 'Battery', "sensor_id": "battery_status_0", "data_getter": d => d.voltage_v * d.current_a }, //TODO: Check getter
+{ "id": 4, "name": 'Wind', "sensor_id": "vehicle_global_position_0", "data_getter": d => d.enu.u }, //TODO: Check
+{ "id": 5, "name": 'Baro', "sensor_id": "sensor_baro_0", "data_getter": d => d.pressure },
+{ "id": 6, "name": 'Gyro', "sensor_id": "sensor_gyro_0", "data_getter": d => d.temperature }] //TODO: Check getter
 
 let sensorLinks = [{
     'source': 0,
@@ -82,9 +82,9 @@ function update_selected_sensor(sensor_index) {
 
     sensorLinks
         .forEach(function (e, idx) {
-            if(linksToIgnore.has(idx)) {
+            if (linksToIgnore.has(idx)) {
                 svg.select(`#link_${idx}`).style('opacity', '1.0')
-            }else {
+            } else {
                 svg.select(`#link_${idx}`).style('opacity', '0.25')
             }
         })
@@ -94,10 +94,104 @@ example_flight_file.retreive_all().then((all_data) => {
     var allSensors = Object.keys(all_data);
     console.log(allSensors)
 
+    generate_sensor_network(sensors, sensorLinks)
+    generate_sensor_datagraph(all_data, sensors)
+
+    //TODO-WIP: Add graph depending of the selected nodes
+
+});
+
+
+function generate_sensor_datagraph(all_data, sensor_list) {
+    let normalize_scales = range(sensor_list.length)
+        .map(i => d3.scaleLinear(d3.extent(all_data[sensor_list[i].sensor_id], sensor_list[i].data_getter), [0, 1]));
+    console.log(normalize_scales)
+    // let sensor_data = [all_data['sensor_gyro_0']]
+    // console.log(all_data['vehicle_gps_position_0'])
+    let sensor_graph_svg = d3.select("#sensor-graphs")
+        .append("svg")
+        .attr('width', '100%')
+        .attr('height', '100%')
+
+    let svg_size = {
+        width: sensor_graph_svg.node().clientWidth,
+        height: sensor_graph_svg.node().clientHeight
+    };
+
+    let padding = {
+        left: 50,
+        right: 10,
+        top: 10,
+        bottom: 20,
+    }
+
+    let g = sensor_graph_svg.append("g")
+        .attr("transform", "translate(" + padding.left + "," + padding.top + ")");
+
+    let clip_path_id = "sensor-graphs-path-line-chart-clip-path";
+    g.append("clipPath")
+        .attr("id", clip_path_id)
+        .append("rect")
+        .attr("width", svg_size.width - padding.left - padding.right)
+        .attr("height", svg_size.height - padding.top - padding.bottom)
+
+    let acceleration_data = all_data.sensor_accel_0;
+    let scale = {
+        x_no_zoom: d3.scaleTime()
+            .domain(d3.extent(acceleration_data, row => row.timestamp))
+            .range([0, svg_size.width - padding.left - padding.right]),
+        y: d3.scaleLinear()
+            .domain([0, 1])
+            .range([svg_size.height - padding.top - padding.bottom, 0])
+    };
+    scale.x = scale.x_no_zoom;
+
+    let axis_x = (g, x) => g.call(d3.axisBottom(x));
+    let axis_y = d3.axisLeft(scale.y);
+
+    g.append("g")
+        .classed("axis", true)
+        .classed("x", true)
+        .attr("transform", "translate(0," + (svg_size.height - padding.bottom - padding.top) + ")")
+        .call(axis_x, scale.x);
+
+    g.append("g")
+        .classed("axis", true)
+        .classed("y", true)
+        .attr("transform", "translate(0,0)")
+        .call(axis_y);
+
+    let lines = range(sensor_list.length)
+        .map(i => d3.line()
+            .x(d => scale.x(d.timestamp))
+            .y(d => scale.y(normalize_scales[i](sensor_list[i].data_getter(d))))
+        );
+
+    let color_scale = d3.scaleOrdinal(d3.schemeCategory10);
+    let update_lines = () => {
+        g.selectAll("path.graph-line")
+            .data(sensor_list)
+            .join(
+                enter => enter.append("path")
+                    .classed("graph-line", true)
+                    .attr("clip-path", `url(#${clip_path_id})`),
+                update => update,
+                exit => exit.remove()
+            )
+            .attr("d", (d, i) => lines[i](all_data[d.sensor_id]))
+            .attr("stroke", (d, i) => color_scale(i))
+            .attr("visibility", (d, i) => "visible");
+        // .attr("visibility", (d, i) => data_is_visible[i] ? "visible" : "hidden");
+
+    };
+    update_lines();
+}
+
+function generate_sensor_network(sensor_list, sensor_links) {
     // Initialize the links
     let link = svg
         .selectAll("line")
-        .data(sensorLinks)
+        .data(sensor_links)
         .enter()
         .append("line")
         .attr("id", (entry, idx) => `link_${idx}`)
@@ -106,7 +200,7 @@ example_flight_file.retreive_all().then((all_data) => {
     // Initialize the nodes
     let node = svg
         .selectAll("circle")
-        .data(sensors)
+        .data(sensor_list)
         .enter()
         .append('g')
         .attr("id", (entry) => `sensor_g_${entry.id}`)
@@ -123,10 +217,10 @@ example_flight_file.retreive_all().then((all_data) => {
     let node_label = node.append('text').text(entry => entry.name).attr('fill', 'white')
 
     // Let's list the force we wanna apply on the network
-    let simulation = d3.forceSimulation(sensors)
+    let simulation = d3.forceSimulation(sensor_list)
         .force("link", d3.forceLink()
             .id(function (d) { return d.id; })
-            .links(sensorLinks)
+            .links(sensor_links)
         )
         .force("charge", d3.forceManyBody().strength(20))
         .force("center", d3.forceCenter(width / 2, height / 2))     // Attract to center of SVG
@@ -142,14 +236,11 @@ example_flight_file.retreive_all().then((all_data) => {
             .attr("y2", function (d) { return d.target.y; });
 
         circle
-            .attr("cx", function (d) { return clamp(d.x, CIRCLE_RADIUS, width); })
-            .attr("cy", function (d) { return clamp(d.y, CIRCLE_RADIUS, height); });
+            .attr("cx", function (d) { return clamp(d.x, CIRCLE_RADIUS / 2.0, width - CIRCLE_RADIUS / 2.0); })
+            .attr("cy", function (d) { return clamp(d.y, CIRCLE_RADIUS / 2.0, height - CIRCLE_RADIUS / 2.0); });
 
         node_label
-            .attr("dx", function (d) { return clamp(d.x - this.getComputedTextLength() / 2.0, CIRCLE_RADIUS, width); })
-            .attr("dy", function (d) { return clamp(d.y, CIRCLE_RADIUS, height); });
+            .attr("dx", function (d) { return clamp(d.x - this.getComputedTextLength() / 2.0, CIRCLE_RADIUS / 2.0, width - CIRCLE_RADIUS / 2.0); })
+            .attr("dy", function (d) { return clamp(d.y, CIRCLE_RADIUS / 2.0, height - CIRCLE_RADIUS / 2.0); });
     }
-
-    //TODO-WIP: Add graph depending of the selected nodes
-
-});
+}
